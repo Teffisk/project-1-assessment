67c21a327e352ec8b4632c9f231b42a3f7d325d6
Teffisk <brad.mcknight91@gmail.com>
2018-12-17 14:57:13 -0800

    fixed red negatives delay

--- js/main.js
+++ js/main.js
@@ -4,29 +4,30 @@ addEventListener("DOMContentLoaded", function(){
 	var addButton = document.querySelector('#add');
 	var subButton = document.querySelector('#subtract');
 	var userInput = document.querySelector('#user-input');
-	//var result = document.querySelector('#result').textContent;
 	var result = 0;
 	addButton.addEventListener('click', function(e){
 		e.preventDefault();
 		add(result, userInput);
+		checkForNegative();
 	});
 	subButton.addEventListener('click', function(e){
 		e.preventDefault();
 		subtract(result, userInput);
+		checkForNegative(result);
 	});
 
 	function add(starting, input) {
+		checkForNegative(starting);
 		result = starting + Number(input.value);
 		document.querySelector('#result').textContent = starting + Number(input.value);
-		checkForNegative(starting);
 		userInput.value = 1;
 
 	};
 
 	function subtract(starting, input) {
+		checkForNegative(starting);
 		result = starting - Number(input.value);
 		document.querySelector('#result').textContent = starting - Number(input.value);
-		checkForNegative(starting);
 		userInput.value = 1;
 	};
 
